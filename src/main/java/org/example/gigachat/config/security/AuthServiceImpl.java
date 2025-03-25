69cb6ca7db2fce82767c9cdf990b557b37c063e0
wojtur <mackowski28@gmail.com>
2025-03-25 07:26:48 +0100

    Quick fix jwt

--- src/main/java/org/example/gigachat/config/security/AuthServiceImpl.java
+++ src/main/java/org/example/gigachat/config/security/AuthServiceImpl.java
@@ -2,9 +2,9 @@ package org.example.gigachat.config.security;
 
 import lombok.AllArgsConstructor;
 import org.example.gigachat.config.security.dto.LoginResponse;
-import org.example.gigachat.config.security.dto.LoginUser;
+import org.example.gigachat.config.security.dto.LoginRequest;
 import org.example.gigachat.config.security.dto.RegisterResponse;
-import org.example.gigachat.config.security.dto.RegisterUser;
+import org.example.gigachat.config.security.dto.RegisterRequest;
 import org.example.gigachat.user.Role;
 import org.example.gigachat.user.User;
 import org.example.gigachat.user.UserRepository;
@@ -23,22 +23,22 @@ public class AuthServiceImpl implements AuthService {
     private final JwtService jwtService;
 
     @Override
-    public Mono<LoginResponse> login(LoginUser loginUser) {
-        return userRepository.findByEmail(loginUser.email())
-                .switchIfEmpty(Mono.error(new IllegalArgumentException("User not found with email: " + loginUser.email())))
-                .filter(u -> passwordEncoder.matches(loginUser.password(), u.getPassword()))
+    public Mono<LoginResponse> login(LoginRequest loginRequest) {
+        return userRepository.findByEmail(loginRequest.email())
+                .switchIfEmpty(Mono.error(new IllegalArgumentException("User not found with email: " + loginRequest.email())))
+                .filter(u -> passwordEncoder.matches(loginRequest.password(), u.getPassword()))
                 .switchIfEmpty(Mono.error(new IllegalArgumentException("Invalid email or password")))
                 .map(u -> new LoginResponse(jwtService.generateToken(u)));
     }
 
     @Override
-    public Mono<RegisterResponse> register(RegisterUser registerUser) {
-        return userRepository.findByEmail(registerUser.email())
+    public Mono<RegisterResponse> register(RegisterRequest registerRequest) {
+        return userRepository.findByEmail(registerRequest.email())
                 .flatMap(existing -> Mono.<RegisterResponse>error(new IllegalArgumentException("User already exists with this email")))
                 .switchIfEmpty(Mono.defer(() -> {
-                    String encodedPassword = passwordEncoder.encode(registerUser.password());
+                    String encodedPassword = passwordEncoder.encode(registerRequest.password());
                     User newUser = new User();
-                    newUser.setEmail(registerUser.email());
+                    newUser.setEmail(registerRequest.email());
                     newUser.setPassword(encodedPassword);
                     newUser.setRoles(Collections.singleton(Role.ROLE_USER));
                     return userRepository.save(newUser)
